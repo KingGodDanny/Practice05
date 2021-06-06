3f72d1e4bde57b1c1275d34e400f206e3fd1c0df
Danny <ctct25@naver.com>
2021-06-06 15:18:37 +0900

    21/06/04 Practice05 ex09 객체지향과제

---
+++ src/com/javaex/ex09/StringUtil.java
@@ -0,0 +1,19 @@
+package com.javaex.ex09;
+
+public class StringUtil {
+    
+ 
+	
+    public static String concatString(String[] strArray){
+    		for(int i = 0; i<3; i++) {
+    			concatString += strArray[i];
+    		}
+    		return concatString;
+    	}
+    	
+    
+   
+    
+}
+    		
+    	  
