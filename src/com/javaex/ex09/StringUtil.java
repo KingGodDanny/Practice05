0e9388be769b25c47010c6f843e0aead01377838
Danny <ctct25@naver.com>
2021-06-06 15:29:59 +0900

    21/06/04 Practice05 ex09 객체지향과제

--- src/com/javaex/ex09/StringUtil.java
+++ src/com/javaex/ex09/StringUtil.java
@@ -5,15 +5,21 @@ public class StringUtil {
  
 	
     public static String concatString(String[] strArray){
-    		for(int i = 0; i<3; i++) {
-    			concatString += strArray[i];
+    	String resultStr = "";
+    		
+    	for(int i = 0; i<strArray.length; i++) {
+    			resultStr = resultStr + strArray[i];
     		}
-    		return concatString;
+    		return resultStr;
     	}
     	
     
    
-    
+  
 }
-    		
+/*숫자의 합을 리턴할땐 그 합을 담을수있는 int sum=0; 이라는 자료형 변수를
+써줬었는데 현재 배열의 0,1,2는 문자열을 담고 있다.
+이들의 문자열들을 합해주기 위해선 합을 담을수 있는 0과 같은 공백이 필요하기
+때문에 String = "";에 담아줘야한다. 
+*/
     	  
